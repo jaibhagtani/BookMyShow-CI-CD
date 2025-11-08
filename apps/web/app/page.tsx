3807b9e22b8150d3330cf93ea539889dfd235846
jaibhagtani <jaibhagtani2@gmail.com>
2025-11-08 13:10:00 +0530

    prisma added, prisma client added to next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,8 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-
+// yaha pr mujhe alag se pnpm install krna padega kyuki, pnpm me @repo/db iske node_modules me aa jayega
+import {client} from "@repo/db/client"
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
   srcDark: string;
