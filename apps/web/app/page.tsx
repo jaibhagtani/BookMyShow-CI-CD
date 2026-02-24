bac09469631d8b570d6e354e34f9eecc76bc7ede
unknown <jaibhagtani>
2026-02-24 18:41:45 +0530

    rectified Errors

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 // yaha pr mujhe alag se pnpm install krna padega kyuki, pnpm me @repo/db iske node_modules me aa jayega
-import { client } from "@repo/db/client"
+import {client} from "@repo/db/client"
 
 export default async function Home() {
   const user = await client.user.findMany();
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <div>
       {
-        user.map((u) => (
+        user.map((u: any) => (
           <div>
             {u?.username} - {u?.password}
           </div>
